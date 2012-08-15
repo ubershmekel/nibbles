68cfb98827bc47ff487ae5f21bb4a60f6b3cf9a8
Yuval Greenfield <ubershmekel@gmail.com>
2012-08-15 17:42:57 +0300

    Remove old stuff

--- nibbles.py
+++ nibbles.py
@@ -2,116 +2,30 @@
 nibbles.py
 
 A tribute to the original QBasic nibbles game in python.
-
-
-Tim: There he is! 
-King Arthur: Where? 
-Tim: There! 
-King Arthur: What? Behind the rabbit? 
-Tim: It *is* the rabbit! 
-King Arthur: You silly sod! 
-Tim: What? 
-King Arthur: You got us all worked up! 
-Tim: Well, that's no ordinary rabbit. 
-King Arthur: Ohh. 
-Tim: That's the most foul, cruel, and bad-tempered rodent you ever set eyes on! 
-Sir Robin: You tit! I soiled my armor I was so scared! 
-Tim: Look, that rabbit's got a vicious streak a mile wide! It's a killer! 
-Sir Galahad: Get stuffed! 
-Tim: He'll do you up a treat, mate. 
-Sir Galahad: Oh, yeah? 
-Sir Robin: You manky Scots git! 
-Tim: I'm warning you! 
-
-Sir Robin: What's he do? Nibble your bum? 
-
-Tim: He's got huge, sharp... er... He can leap about. Look at the bones! 
-King Arthur: Go on, Bors. Chop his head off! 
-Sir Bors: Right! Silly little bleeder. One rabbit stew comin' right up! 
 """
 
+import Tkinter
 import random
 import time
-import turtle
 
-APPLE_SIZE = 20
-
-
-PLAYING = True
-APPLE_LOC = None
-
-def up():
-    tod.setheading(90)
-
-def down():
-    tod.setheading(270)
-
-def left():
-    tod.setheading(180)
-
-def right():
-    tod.setheading(0)
-
-def stop():
-    global PLAYING
-    PLAYING = False
-    
-"""
-tod = turtle.Turtle()
-tod.speed(0)
-
-screen = tod.getscreen()
-cv = screen.getcanvas()
-screen.title('NIBBLES')
-screen.onkey(left, 'Left')
-screen.onkey(up, 'Up')
-screen.onkey(right, 'Right')
-screen.onkey(down, 'Down')
-screen.onkey(stop, 'q')
-screen.listen()
-
-L, R, B, T = -screen.window_width() // 2, screen.window_width() // 2, -screen.window_height() // 2, screen.window_height() // 2
-
-
-def main():
-    global APPLE_LOC
-    global PLAYING
-    
-    while PLAYING:
-        tod.forward(2)
-        if APPLE_LOC is None:
-            x = random.randint(L + APPLE_SIZE, R - APPLE_SIZE)
-            y = random.randint(B + APPLE_SIZE, T - APPLE_SIZE)
-            APPLE_LOC = x, y
-            cv.create_rectangle((x, y, x + APPLE_SIZE, y + APPLE_SIZE), fill="red")
-            x = x
-            y = -y
-        
-        todx, tody = tod.xcor(), tod.ycor()
-        if (L < todx < R) and (B < tody < T):
-            pass
-        else:
-            print 'out of bounds'
-            PLAYING = False
-        
-        if x < todx < x + APPLE_SIZE and y - APPLE_SIZE < tody < y:
-            print 'got the apple'
-        #print todx, tody, x, y
-        #print x/screen.xscale, y/screen.yscale, todx, tody
-
-main()
-"""
-import Tkinter
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 FPS = 30.0
 
 UP, DOWN, LEFT, RIGHT = (0, -1), (0, 1), (-1, 0), (1, 0)
+SNAKE_SIZE = 20
+PLAYING = True
+APPLE_LOC = None
+
+def stop(event):
+    global PLAYING
+    PLAYING = False
 
 class Snake:
     def __init__(self, game):
         self.game = game
-        self.r = game.canvas.create_rectangle(50, 25, 150, 75, fill="blue")
+        x, y = 0, 0
+        self.r = game.canvas.create_rectangle(x, y, x + SNAKE_SIZE, y + SNAKE_SIZE, fill="blue")
         self.direction = RIGHT
         self.bind_keys()
 
